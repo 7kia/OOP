bcd9439431f4c25e106d0a9e3ba7182fff088317
7kia <il7kia@yandex.ru>
2016-02-09 20:28:18 +0400

    OOP Lab1-2(7 variant) v3 Add support negative numbers.

--- Lab1-1/Task1.cpp
+++ Lab1-1/Task1.cpp
@@ -3,8 +3,27 @@
 
 
 /*
-
 Лабораторная работа 1. 1 задание 4 вариант
+
+Разработайте программу replace.exe, выполняющую замену подстроки 
+в текстовом файле на другую строку, и записывающей результат в
+выходной файл (отличный от входного). 
+
+Формат командной строки:
+replace.exe <input file><output file><search string><replace string>
+
+Программа должна корректно обрабатывать ошибки, связанные с 
+открытием входных и выходных файлов. Искомая строка не может быт
+пустой.
+
+Размеры входных и выходных файлов ограничены 2 Гб. Размеры искомой
+строки и строки-заменителя не ограничены.
+Внимание, программа корректно должна обрабатывать ситуацию, когда 
+длина искомой строки равна нулю. В этом случае замены символов 
+производиться не должно.Также корректно должна обрабатываться 
+ситуация с многократным вхождением искомой строки в 
+строку-заменитель (например, замена строки «ма» на «мама» 
+не должна приводить к зацикливанию).
 */
 
 #include "stdafx.h"
@@ -25,9 +44,9 @@ bool CheckLoadFile(FILE **file , char const *name , char const *mode)
 }
 
 
-void resetString(char* array, int size, char value)
+void resetString(char* array, char value)
 {
-	for (size_t id = 0; id < size; id++)
+	for (size_t id = 0; id < strlen(array); id++)
 	{
 		array[id] = value;
 	}
@@ -37,7 +56,19 @@ bool checkParametrs(int argc , char *argv[])
 {
 	if (argc == 5)
 	{
+		for (int i = 1; i < 5; i++)
+		{
+			if (!isalpha(*argv[i]) && !isdigit(*argv[i]))
+			{
+				std::cout << "Arguments must consist letters or(and) numbers!!! " << *argv[i] <<  std::endl;
+				return false;
+			}
+		}
 		return true;
+	}
+	else
+	{
+		std::cout << "Not enough amount parametrs!!! " << std::endl;
 	}
 
 	return false;
@@ -66,38 +97,40 @@ int main(int argc , char *argv[])
 															&& CheckLoadFile(&outputFile , nameOutputFile , "w");
 		if (successfulLoadFiles)
 		{
-			unsigned int idCheckSymbol = 0;
-			unsigned int countСoincidence = 0;
 			int idFirstEqualSymbol = RESET_VALUE;
 
 			char charBuffer[SIZE_CHAR_BUFFER];
+			char equalSymbols[SIZE_CHAR_BUFFER];
+
 			char equalSymbol = '\0';
-			resetString(charBuffer , SIZE_CHAR_BUFFER , '\0');
-			//resetString(equalSymbols , SIZE_CHAR_BUFFER , '\0');
+			resetString(charBuffer, '\0');
+			resetString(equalSymbols , '\0');
 
 			while (!feof(inputFile))
 			{
 				fgets(charBuffer , SIZE_CHAR_BUFFER , inputFile);
 
+				unsigned int idCheckSymbol = 0;
+				unsigned int countСoincidence = 0;
+				bool wasEqualSymbol = false;
 				int i = 0;
 				while((i < SIZE_CHAR_BUFFER) && (charBuffer[i] != '\0'))
 				{
 					if (charBuffer[i] == searchString[idCheckSymbol])
 					{
-
 						if (idFirstEqualSymbol == RESET_VALUE)
 						{
-							idFirstEqualSymbol = i;
-							equalSymbol = charBuffer[i];
+							idFirstEqualSymbol = i + 1;
 						}
-
+						equalSymbols[idCheckSymbol] = charBuffer[i];
+						equalSymbols[idCheckSymbol + 1] = '\0';
 						
 						idCheckSymbol++;
 						if (idCheckSymbol == strlen(searchString))
 						{
 							countСoincidence++;
 							idCheckSymbol = 0;
-							equalSymbol = '\0';
+							resetString(equalSymbols , '\0');
 
 							fputs(replaceString , outputFile);
 						}
@@ -105,11 +138,18 @@ int main(int argc , char *argv[])
 					}
 					else
 					{
-						if (equalSymbol != '\0')
+						bool wasEqualSymbol = equalSymbols[0] != '\0';
+						if (wasEqualSymbol)
 						{
-							fputc(equalSymbol , outputFile);
-							i = idFirstEqualSymbol + 1;
-							equalSymbol = '\0';
+							//for (int counter = 0; counter < strlen(equalSymbols); counter++)
+							//{
+								fputs(equalSymbols , outputFile);
+
+							//}
+
+							i -= (strlen(equalSymbols) - 1);
+							std::cout << idFirstEqualSymbol << std::endl;
+							resetString(equalSymbols , '\0');
 						}
 						else
 						{
@@ -117,12 +157,12 @@ int main(int argc , char *argv[])
 							i++;
 						}
 
+						idFirstEqualSymbol = RESET_VALUE;
 						idCheckSymbol = 0;
 					}
-					//i++;
 				}
 
-				resetString(charBuffer , SIZE_CHAR_BUFFER , '\0');
+				resetString(charBuffer, '\0');
 			}
 
 		}
@@ -132,7 +172,7 @@ int main(int argc , char *argv[])
 	}
 	else
 	{
-		std::cout << "Not enough amount parametrs!!! " << argc << std::endl;
+		std::cout << "Not enough amount parametrs!!! " << std::endl;
 	}
 
 	std::cout << "Press any key for exit..." << std::endl;
--- Lab1-2/Lab1-2.cpp
+++ Lab1-2/Lab1-2.cpp
@@ -29,7 +29,6 @@ radix.exe 16 10 1F
 */
 #include "stdafx.h"
 
-static const int SIZE_STRING = 255;
 static const int ERROR_CODE = -1;
 
 template<typename T>
@@ -58,19 +57,22 @@ int CharToInt(char symbol)
 	return ERROR_CODE;
 }
 
+int DefineStartValue(int defaultValue, int alternateValue, bool condition)
+{
+	if (condition)
+	{
+		return alternateValue;
+	}
+	return defaultValue;
+}
 
 bool checkInputNumber(char* number , int numberInputNotation)
 {
-	int start = 0;
-
-	if (number[0] == '-')
+	int valueSymbol;
+	for (int i = DefineStartValue(0, 1, number[0] == '-'); i < strlen(number); i++)
 	{
-		start = 1;
-	}
-
-	for (int i = start; i < strlen(number); i++)
-	{
-		if (CharToInt(number[i]) > numberInputNotation)
+		valueSymbol = CharToInt(number[i]);
+		if ((valueSymbol > numberInputNotation) || (valueSymbol == ERROR_CODE))
 		{
 			return false;
 		}
@@ -83,36 +85,95 @@ bool checkParametrs(int argc , char *argv[])
 	if (argc == 4)
 	{
 		bool isError = false;
-		for (int i = 1; i < argc; i++)
-		{
-			if (!isdigit(*argv[1]))
-			{
-				std::cout << "Number input system notation is not number!!!" << argc << std::endl;
-				isError = true;
-			}
-			if (!isdigit(*argv[2]))
-			{
-				std::cout << "Number output system notation is not number!!!" << argc << std::endl;
-				isError = true;
-			}
 
-			if (!isdigit(*argv[3]) && !isalpha(*argv[3]))
-			{
-				std::cout << "Incorrect translate number!!!" << argc << std::endl;
-				isError = true;
-			}
-			else if(!checkInputNumber(argv[3], atoi(argv[1])))
-			{
-				std::cout << "Use incorrect number notation or incorrect retranslate number!!!" << argc << std::endl;
-				isError = true;
-			}
+		if (!isdigit(*argv[1]))
+		{
+			std::cout << "Number input system notation is not number!!!" << argc << std::endl;
+			isError = true;
 		}
+		if (!isdigit(*argv[2]))
+		{
+			std::cout << "Number output system notation is not number!!!" << argc << std::endl;
+			isError = true;
+		}
+
+		if (!checkInputNumber(argv[3] , atoi(argv[1])))
+		{
+			std::cout << "Use incorrect number notation or incorrect retranslate number!!!" << argc << std::endl;
+			isError = true;
+		}
+
 		return !isError;
-		
 	}
 	std::cout << "Not enough amount parametrs!!! " << argc << std::endl;
 
 	return false;
+}
+
+int TranslateStringToNumber(std::string str , int numberNotation)
+{
+	int multiplier = 0;
+	int degree = 0;
+	int result = 0;
+
+	bool isSigned = (str[0] == '-');
+	int endValue = DefineStartValue(0 , 1 , str[0] == '-');
+	for (int i = int(str.size()) - 1; i >= endValue; i--)
+	{
+		multiplier = CharToInt(str[i]);
+
+		result += multiplier * int(pow(numberNotation , degree));
+		degree++;
+	}
+
+	if (isSigned)
+	{
+		result *= -1;
+	}
+
+	return result;
+}
+
+std::string TranslateIntToString(int result, int numberOutputNotation)
+{
+	std::string outputNumber;
+
+	int divider = numberOutputNotation;
+	int remain = 1;
+	if (result != 0)
+	{
+		bool isSigned = result < 0;
+
+		unsigned int anbsoluteValue = abs(result);
+		while (anbsoluteValue > 0)
+		{
+			remain = anbsoluteValue % numberOutputNotation;
+
+			anbsoluteValue /= numberOutputNotation;
+
+			if (IsInDiaposon(remain , 0 , 9))
+			{
+				outputNumber.insert(outputNumber.begin() , int('0') + remain);
+			}
+			else
+			{
+				outputNumber.insert(outputNumber.begin() , int('A') + remain - 10);
+			}
+
+		}
+
+		if (isSigned)
+		{
+			outputNumber.insert(outputNumber.begin() , '-');
+		}
+
+	}
+	else
+	{
+		outputNumber = '0';
+	}
+
+	return outputNumber;
 }
 
 int main(int argc , char *argv[])
@@ -122,54 +183,13 @@ int main(int argc , char *argv[])
 	{
 		unsigned int numberInputNotation = atoi(argv[1]);
 		unsigned int numberOutputNotation = atoi(argv[2]);
-		unsigned int degree = 0;
-		int result = 0;
-		std::string inputNumber = argv[3];
-		
-		int multiplier = 0;
-		for (int i = int(inputNumber.size()) - 1; i >= 0; i--)
-		{
-			multiplier = CharToInt(inputNumber[i]);
-			assert(multiplier != ERROR_CODE);
 
-			result += multiplier * int(pow(numberInputNotation , degree));
-			degree++;
-		}
-
-		std::string outputNumber;
-		int copyResult = result;
-		int divider = numberOutputNotation;
-		int remain = 1;
-		degree = 2;
-		if (copyResult > 0)
-		{
-			
-			while (copyResult > 0)
-			{
-				remain = copyResult % numberOutputNotation;
-
-				copyResult /= numberOutputNotation;
-
-				if (IsInDiaposon(remain, -9, 9))
-				{
-					outputNumber.insert(outputNumber.begin(), int('0') + remain);
-				}
-				else
-				{
-					outputNumber.insert(outputNumber.begin() , int('A') + remain - 10);
-				}
-
-			}
-
-			if (inputNumber[0] == '-')
-			{
-				outputNumber.insert(outputNumber.begin() , '-');
-			}
-
-		}	
+		std::string inputNumber = argv[3];		
+		int result = TranslateStringToNumber(inputNumber , numberInputNotation);
+		std::string outputNumber = TranslateIntToString(result, numberOutputNotation);
 
 		std::cout << "Result = " << outputNumber << std::endl;
-
+		std::cout << "In desimal notation = " << result << std::endl;
 	}
 
 	std::cout << "Press any key for exit..." << std::endl;
@@ -177,4 +197,3 @@ int main(int argc , char *argv[])
 
    return 0;
 }
-
