fdfa2db9ffc8673c98ca5b7b91e6e1015c61ecb2
7kia <il7kia@yandex.ru>
2016-02-10 08:47:39 +0400

    OOOP Lab1-1(4 variant) v4 Program use new algoritm.

--- Lab1-1/Task1.cpp
+++ Lab1-1/Task1.cpp
@@ -3,27 +3,8 @@
 
 
 /*
+
 Лабораторная работа 1. 1 задание 4 вариант
-
-Разработайте программу replace.exe, выполняющую замену подстроки 
-в текстовом файле на другую строку, и записывающей результат в
-выходной файл (отличный от входного). 
-
-Формат командной строки:
-replace.exe <input file><output file><search string><replace string>
-
-Программа должна корректно обрабатывать ошибки, связанные с 
-открытием входных и выходных файлов. Искомая строка не может быт
-пустой.
-
-Размеры входных и выходных файлов ограничены 2 Гб. Размеры искомой
-строки и строки-заменителя не ограничены.
-Внимание, программа корректно должна обрабатывать ситуацию, когда 
-длина искомой строки равна нулю. В этом случае замены символов 
-производиться не должно.Также корректно должна обрабатываться 
-ситуация с многократным вхождением искомой строки в 
-строку-заменитель (например, замена строки «ма» на «мама» 
-не должна приводить к зацикливанию).
 */
 
 #include "stdafx.h"
@@ -44,9 +25,9 @@ bool CheckLoadFile(FILE **file , char const *name , char const *mode)
 }
 
 
-void resetString(char* array, char value)
+void resetString(char* array, int size, char value)
 {
-	for (size_t id = 0; id < strlen(array); id++)
+	for (size_t id = 0; id < size; id++)
 	{
 		array[id] = value;
 	}
@@ -56,19 +37,7 @@ bool checkParametrs(int argc , char *argv[])
 {
 	if (argc == 5)
 	{
-		for (int i = 1; i < 5; i++)
-		{
-			if (!isalpha(*argv[i]) && !isdigit(*argv[i]))
-			{
-				std::cout << "Arguments must consist letters or(and) numbers!!! " << *argv[i] <<  std::endl;
-				return false;
-			}
-		}
 		return true;
-	}
-	else
-	{
-		std::cout << "Not enough amount parametrs!!! " << std::endl;
 	}
 
 	return false;
@@ -97,40 +66,38 @@ int main(int argc , char *argv[])
 															&& CheckLoadFile(&outputFile , nameOutputFile , "w");
 		if (successfulLoadFiles)
 		{
+			unsigned int idCheckSymbol = 0;
+			unsigned int countСoincidence = 0;
 			int idFirstEqualSymbol = RESET_VALUE;
 
 			char charBuffer[SIZE_CHAR_BUFFER];
-			char equalSymbols[SIZE_CHAR_BUFFER];
-
 			char equalSymbol = '\0';
-			resetString(charBuffer, '\0');
-			resetString(equalSymbols , '\0');
+			resetString(charBuffer , SIZE_CHAR_BUFFER , '\0');
+			//resetString(equalSymbols , SIZE_CHAR_BUFFER , '\0');
 
 			while (!feof(inputFile))
 			{
 				fgets(charBuffer , SIZE_CHAR_BUFFER , inputFile);
 
-				unsigned int idCheckSymbol = 0;
-				unsigned int countСoincidence = 0;
-				bool wasEqualSymbol = false;
 				int i = 0;
 				while((i < SIZE_CHAR_BUFFER) && (charBuffer[i] != '\0'))
 				{
 					if (charBuffer[i] == searchString[idCheckSymbol])
 					{
+
 						if (idFirstEqualSymbol == RESET_VALUE)
 						{
-							idFirstEqualSymbol = i + 1;
+							idFirstEqualSymbol = i;
+							equalSymbol = charBuffer[i];
 						}
-						equalSymbols[idCheckSymbol] = charBuffer[i];
-						equalSymbols[idCheckSymbol + 1] = '\0';
+
 						
 						idCheckSymbol++;
 						if (idCheckSymbol == strlen(searchString))
 						{
 							countСoincidence++;
 							idCheckSymbol = 0;
-							resetString(equalSymbols , '\0');
+							equalSymbol = '\0';
 
 							fputs(replaceString , outputFile);
 						}
@@ -138,18 +105,11 @@ int main(int argc , char *argv[])
 					}
 					else
 					{
-						bool wasEqualSymbol = equalSymbols[0] != '\0';
-						if (wasEqualSymbol)
+						if (equalSymbol != '\0')
 						{
-							//for (int counter = 0; counter < strlen(equalSymbols); counter++)
-							//{
-								fputs(equalSymbols , outputFile);
-
-							//}
-
-							i -= (strlen(equalSymbols) - 1);
-							std::cout << idFirstEqualSymbol << std::endl;
-							resetString(equalSymbols , '\0');
+							fputc(equalSymbol , outputFile);
+							i = idFirstEqualSymbol + 1;
+							equalSymbol = '\0';
 						}
 						else
 						{
@@ -160,9 +120,10 @@ int main(int argc , char *argv[])
 						idFirstEqualSymbol = RESET_VALUE;
 						idCheckSymbol = 0;
 					}
+					//i++;
 				}
 
-				resetString(charBuffer, '\0');
+				resetString(charBuffer , SIZE_CHAR_BUFFER , '\0');
 			}
 
 		}
@@ -172,7 +133,7 @@ int main(int argc , char *argv[])
 	}
 	else
 	{
-		std::cout << "Not enough amount parametrs!!! " << std::endl;
+		std::cout << "Not enough amount parametrs!!! " << argc << std::endl;
 	}
 
 	std::cout << "Press any key for exit..." << std::endl;
