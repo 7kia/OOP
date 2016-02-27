4cc9f490a2938a630b363a566df79739be12d72e
7kia <il7kia@yandex.ru>
2016-02-27 12:39:28 +0400

    Update Tests.

--- Lab1-3/Tests/Lab1-3Tests.bat
+++ Lab1-3/Tests/Lab1-3Tests.bat
@@ -7,6 +7,9 @@ IF NOT ERRORLEVEL 1 GOTO err
 Lab1-3.exe vc output.txt ma MAMA
 IF NOT ERRORLEVEL 1 GOTO err
 
+Lab1-3.exe matrixE.txt
+IF NOT ERRORLEVEL 1 GOTO err
+
 Lab1-3.exe matrix.txt >> out.txt
 IF ERRORLEVEL 1 GOTO err
 FC /b out.txt CheckData/right.txt
@@ -15,6 +18,16 @@ IF ERRORLEVEL 1 GOTO err
 Lab1-3.exe matrix2.txt >> out2.txt
 IF ERRORLEVEL 1 GOTO err
 FC /b out2.txt CheckData/right2.txt
+IF ERRORLEVEL 1 GOTO err
+
+Lab1-3.exe matrix3.txt >> out3.txt
+IF ERRORLEVEL 1 GOTO err
+FC /b out3.txt CheckData/right3.txt
+IF ERRORLEVEL 1 GOTO err
+
+Lab1-3.exe matrix4.txt >> out4.txt
+IF ERRORLEVEL 1 GOTO err
+FC /b out4.txt CheckData/right4.txt
 IF ERRORLEVEL 1 GOTO err
 
 ECHO Program testing succeeded :-) 
