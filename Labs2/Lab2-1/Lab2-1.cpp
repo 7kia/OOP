74006aa992727128a74b094ac05c0bbc21ef95da
7kia <il7kia@yandex.ru>
2016-02-29 21:29:44 +0400

    Lab2-1 variant4 v0.1 Program do necessary compute.

--- Labs2/Lab2-1/Lab2-1.cpp
+++ Labs2/Lab2-1/Lab2-1.cpp
@@ -7,17 +7,34 @@
 #include <sstream>
 #include <iterator>
 #include <vector>
+#include <algorithm>
+#include <boost/range/algorithm/transform.hpp>
 
-std::vector<int> ReadFromConsole()
+/*
+2 лабораторная 1 задание
+Вариант 2
+Каждый элемент массива должен быть умножен на минимальный элемент исходного массива
+*/
+
+
+std::vector<double> ReadFromConsole()
 {
 	return { std::istream_iterator<double>(std::cin) , (std::istream_iterator<double>()) };
 }
 
 int main()
-{
-	std::vector<int> vector;
+{	
+	std::vector<double> vector;
 
 	vector = ReadFromConsole();
+	auto minValue = std::min_element(vector.begin(), vector.end());
+
+	auto Multiply = [minValue](double source) {
+		return source * *minValue;
+	};
+
+	boost::transform(vector, vector.begin(), Multiply);
+
     return 0;
 }
 
