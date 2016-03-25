1eee7dd88da97e0d590ac6a47d5641e6c5f8f075
7kia <il7kia@yandex.ru>
2016-03-25 14:46:48 +0400

    Add tests for check transform functions and Intersect().

--- Lab3/Rectangle/Tests/Tests.cpp
+++ Lab3/Rectangle/Tests/Tests.cpp
@@ -44,6 +44,74 @@ BOOST_AUTO_TEST_CASE(Check_rectangle_set_and_get_functions)
 	BOOST_CHECK(rect.GetRight() == (x + width));
 }
 
+BOOST_AUTO_TEST_CASE(Check_rectangle_transform_functions)
+{
+	int x = 1;
+	int y = 1;
+	int width = 2;
+	int height = 3;
+	CRectangle rect(Vector2I(x, y), width, height);
+
+	int dx = 1;
+	int dy = 1;
+	rect.Move(dx, dy);
+	BOOST_CHECK(rect.GetLeftTopPoint() == Vector2I(x + dx, y + dy));
+	BOOST_CHECK(rect.GetWidth() == width);
+	BOOST_CHECK(rect.GetHeight() == height);
+
+	BOOST_CHECK(rect.GetArea() == (width * height));
+	BOOST_CHECK(rect.GetPerimeter() == ((width + height) * 2));
+
+	BOOST_CHECK(rect.GetTop() == (y + dy));
+	BOOST_CHECK(rect.GetBottom() == (y + height + dy));
+	BOOST_CHECK(rect.GetLeft() == (x + dx));
+	BOOST_CHECK(rect.GetRight() == (x + width + dx));
+
+	int sx = 2;
+	int sy = 3;
+	rect.Scale(sx, sy);
+	BOOST_CHECK(rect.GetLeftTopPoint() == Vector2I(x + dx, y + dy));
+	BOOST_CHECK(rect.GetWidth() == (width * sx));
+	BOOST_CHECK(rect.GetHeight() == (height * sy));
+
+	BOOST_CHECK(rect.GetArea() == (width * height * sx * sy));
+	BOOST_CHECK(rect.GetPerimeter() == (((width * sx) + (height * sy)) * 2));
+
+	BOOST_CHECK(rect.GetTop() == (y + dy));
+	BOOST_CHECK(rect.GetBottom() == (y + (height * sy) + dy));
+	BOOST_CHECK(rect.GetLeft() == (x + dx));
+	BOOST_CHECK(rect.GetRight() == (x + (width * sx) + dx));
+}
+
+BOOST_AUTO_TEST_CASE(Check_CRectangle_Intersect)
+{
+	int x = 1;
+	int y = 1;
+	int width = 2;
+	int height = 3;
+	CRectangle rect(Vector2I(x, y), width, height);
+	CRectangle rect2(Vector2I(x, y), width, height);
+
+	int dx = 1;
+	int dy = 1;
+	rect2.Move(dx, dy);
+	BOOST_CHECK(rect.Intersect(rect2) == true);
+	BOOST_CHECK(rect.GetWidth() == width);
+	BOOST_CHECK(rect.GetHeight() == height);
+
+
+	rect2.Move(width - dx, height - dy);
+	BOOST_CHECK(rect.Intersect(rect2) == true);
+	BOOST_CHECK(rect.GetWidth() == width);
+	BOOST_CHECK(rect.GetHeight() == height);
+
+	rect2.Move(width - dx, height - dy);
+	BOOST_CHECK(rect.Intersect(rect2) == false);
+	BOOST_CHECK(rect.GetWidth() == 0);
+	BOOST_CHECK(rect.GetHeight() == 0);
+
+}
+
 BOOST_AUTO_TEST_SUITE_END()
 
 
