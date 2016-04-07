1806194d707eaf9e70a29de3f0cae9b8835a78ac
7kia <il7kia@yandex.ru>
2016-04-07 12:32:52 +0400

    ShapeDrawer draw circle and points.

--- Lab4/ShapeDrawer/RenderShapes.cpp
+++ Lab4/ShapeDrawer/RenderShapes.cpp
@@ -19,18 +19,21 @@ listRenderShapes ConvertDataShapesToRenderShapes(listDataShapes & data)
 		type = shape->GetType();
 		if (type == "Point")
 		{
-			
+			renderShapes.push_back(ConvertInRenderPoint(dynamic_cast<MyCPoint*>(shape.get())));
 		}
 		else if(type == "Line")
 		{
-			renderShapes.push_back(ConvertDataLineInRenderLine(dynamic_cast<CLineSegment*>(shape.get())));
+			renderShapes.push_back(ConvertInRenderLine(dynamic_cast<CLineSegment*>(shape.get())));
 		}
-
+		else if (type == "Circle")
+		{
+			renderShapes.push_back(ConvertInRenderCircle(dynamic_cast<CCircle*>(shape.get())));
+		}
 	}
 	return renderShapes;
 }
 
-std::shared_ptr<sf::RectangleShape> ConvertDataLineInRenderLine(const CLineSegment *data)
+std::shared_ptr<sf::RectangleShape> ConvertInRenderLine(const CLineSegment *data)
 {
 	std::shared_ptr<sf::RectangleShape> line(new sf::RectangleShape);
 
@@ -40,14 +43,48 @@ std::shared_ptr<sf::RectangleShape> ConvertDataLineInRenderLine(const CLineSegme
 	line->setOrigin(DefaultParametresShape::originLine);
 	line->setPosition(data->GetPositiionFirstPoint());
 
-	float angle = atan2(DefaultParametresShape::originLine.x, DefaultParametresShape::originLine.y);
-
 	sf::Vector2f coordinateSecondPointInZeroSystemCoordinates = data->GetPositiionSecondPoint();
 	coordinateSecondPointInZeroSystemCoordinates -= data->GetPositiionFirstPoint();
 
-	angle -= angle + atan2(coordinateSecondPointInZeroSystemCoordinates.x, coordinateSecondPointInZeroSystemCoordinates.y);
+	float angle = (atan2(coordinateSecondPointInZeroSystemCoordinates.x, coordinateSecondPointInZeroSystemCoordinates.y)) * 180 / M_PI;
+	if (angle < 0) {
+		angle += 360;
+	}
 
 	line->setRotation(angle);
 
 	return line;
+}
+
+std::shared_ptr<sf::CircleShape> ConvertInRenderPoint(const MyCPoint * data)
+{
+	std::shared_ptr<sf::CircleShape> circle(new sf::CircleShape);
+
+	SColor fillColor = data->GetFillColor();
+	circle->setFillColor(sf::Color(fillColor.red, fillColor.blue, fillColor.green));
+
+	circle->setOrigin(DefaultParametresShape::radiusePoint / 2, DefaultParametresShape::radiusePoint / 2);
+	circle->setPosition(data->GetPosition());
+
+	circle->setRadius(DefaultParametresShape::radiusePoint);
+
+	return circle;
+}
+
+std::shared_ptr<sf::CircleShape> ConvertInRenderCircle(const CCircle * data)
+{
+	std::shared_ptr<sf::CircleShape> circle(new sf::CircleShape);
+
+	SColor fillColor = data->GetFillColor();
+	circle->setFillColor(sf::Color(fillColor.red, fillColor.blue, fillColor.green));
+
+	SColor outlineColor = data->GetOutlineColor();
+	circle->setOutlineColor(sf::Color(outlineColor.red, outlineColor.blue, outlineColor.green));
+	circle->setOutlineThickness(DefaultParametresShape::thiknessLine);
+	circle->setOrigin(data->GetRadiuse(), data->GetRadiuse());
+	circle->setPosition(data->GetPosition());
+
+	circle->setRadius(data->GetRadiuse());
+
+	return circle;
 }
--- Lab4/ShapeDrawer/RenderShapes.h
+++ Lab4/ShapeDrawer/RenderShapes.h
@@ -11,10 +11,12 @@ namespace DefaultParametresShape
 {
 	static const float thiknessLine = 5;
 	static const sf::Vector2f originLine = { DefaultParametresShape::thiknessLine / 2.f, 0.f };
+	static const float radiusePoint = 2.f;
 };
 
 listRenderShapes ConvertDataShapesToRenderShapes(listDataShapes & data);
-std::shared_ptr<sf::RectangleShape> ConvertDataLineInRenderLine(const CLineSegment *data);
-
+std::shared_ptr<sf::CircleShape> ConvertInRenderPoint(const MyCPoint * data);
+std::shared_ptr<sf::RectangleShape> ConvertInRenderLine(const CLineSegment *data);
+std::shared_ptr<sf::CircleShape> ConvertInRenderCircle(const CCircle *data);
 
 void Render(sf::RenderWindow & window, listRenderShapes &shapes);
