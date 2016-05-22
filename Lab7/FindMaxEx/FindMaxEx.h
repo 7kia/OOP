a355884d99581c04ac92ef0fec66051dbda2d0d4
7kia <il7kia@yandex.ru>
2016-05-22 18:46:12 +0400

    Add basic for lab "FindMaxEx"

---
+++ Lab7/FindMaxEx/FindMaxEx.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <vector>
+#include <iostream>
+#include <sstream>
+#include <iterator>
+#include <algorithm>
+#include <boost/phoenix.hpp>
+#include <boost/range/algorithm/transform.hpp>
+
+void ProcessVector(std::vector<double> &vector);
