5047910020c5fef309bd7b2e3433aedadd4d081c
7kia <il7kia@yandex.ru>
2016-06-01 10:15:47 +0400

    добавлены недостающие файлы

--- Lab7/FindMaxEx/FindMaxExDeclaration.h
+++ Lab7/FindMaxEx/FindMaxExDeclaration.h
@@ -8,6 +8,7 @@ bool FindMax(std::vector<T> const& array
 			, T &maxValue
 			, Less const& less = Less());
 */
+
 bool FindMax(const std::vector<const char *> & array
-	, char &maxValue
-	, std::function<bool(const char, const char)> const& less);
+			, char &maxValue
+			, std::function<bool(const char, const char)> const& less);
